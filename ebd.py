97045f44572f6ef29f3f332370013eb8c8af596b
Danilo Silva <danilocgsilva@gmail.com>
2020-01-06 23:15:08 -0300

    Not showing loading module error

--- ebd.py
+++ ebd.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-
-from bin.Arguments import Arguments
+from objs.Arguments import Arguments
 
 arguments = Arguments()
 print('The first argument is ' + arguments.get_argument())
--- objs/Arguments.py
+++ objs/Arguments.py
@@ -5,6 +5,5 @@ class Arguments:
     def __init__(self):
         self.argument = sys.argv[1]
 
-
-    def get_argument(self) : str
+    def get_argument(self):
         return self.argument
