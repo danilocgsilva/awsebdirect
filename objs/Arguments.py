0635e2e6aad2bad48a3042023d48e0bf63235ec0
Danilo Silva <danilocgsilva@gmail.com>
2020-01-07 07:35:24 -0300

    Script does not breaks if no argument is given

--- objs/Arguments.py
+++ objs/Arguments.py
@@ -3,7 +3,11 @@ import sys
 class Arguments:
 
     def __init__(self):
-        self.argument = sys.argv[1]
+        try:
+            self.argument = sys.argv[1]
+        except IndexError as ie:
+            self.argument = ''
+
 
     def get_argument(self):
         return self.argument
