9be383f20d400520834e82189ca9e22ff1fc4942
Danilo Silva <danilocgsilva@gmail.com>
2020-01-06 23:06:58 -0300

    Starting the body of logic

---
+++ bin/Arguments.py
@@ -0,0 +1,10 @@
+import sys
+
+class Arguments:
+
+    def __init__(self):
+        self.argument = sys.argv[1]
+
+
+    def get_argument(self) : str
+        return self.argument
--- ebd.py
+++ ebd.py
@@ -1,1 +1,6 @@
-print('Hello World!')
+#!/usr/bin/python3
+
+from bin.Arguments import Arguments
+
+arguments = Arguments()
+print('The first argument is ' + arguments.get_argument())
