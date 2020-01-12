969251e18ecb6478e1b9ee0d45bf99a6b856ebac
Danilo Silva <danilocgsilva@gmail.com>
2020-01-12 10:40:36 -0300

    Removed environment test and variable check

--- objs/Environment.py
+++ objs/Environment.py
@@ -2,10 +2,5 @@ import os
 
 class Environment:
 
-    def __init__(self):
-        if os.environ['AWS_PROFILE'] == None:
-            raise Exception('There\'s no way to know which profile should be used.')
-
-
     def get_region(self) -> str:
         return "us-east-1"
