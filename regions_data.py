a7894599b1ccc74dfd5ee83ca64179c1cf4d663e
Danilo Silva <danilocgsilva@gmail.com>
2020-01-12 16:25:43 -0300

    Region human readable data written

---
+++ regions_data.py
@@ -0,0 +1,21 @@
+regions_data = {
+    "us-east-2":"Leste dos EUA (Ohio)",
+    "us-east-1":"Leste dos EUA (Norte da Virgínia)",
+    "us-west-1":"Oeste dos EUA (Norte da Califórnia)",
+    "us-west-2":"Oeste dos EUA (Oregon)",
+    "ap-east-1": "Ásia-Pacífico (Hong Kong)",
+    "ap-south-1":"Ásia Pacífico (Mumbai)",
+    "ap-northeast-3":"Ásia-Pacífico (Osaka – Local)",
+    "ap-northeast-2":"Ásia-Pacífico (Seul)",
+    "ap-southeast-1":"Ásia-Pacífico (Cingapura)",
+    "ap-southeast-2":"Ásia-Pacífico (Sydney)",
+    "ap-northeast-1":"Ásia-Pacífico (Tóquio)",
+    "ca-central-1":"Canadá (Central)",
+    "eu-central-1":"Europa (Frankfurt)",
+    "eu-west-1":"Europa (Irlanda)",
+    "eu-west-2":"Europa (Londres)",
+    "eu-west-3":"Europa (Paris)",
+    "eu-north-1":"Europa (Estocolmo)",
+    "me-south-1":"Oriente Médio (Bahrein)",
+    "sa-east-1":"América do Sul (São Paulo)"
+}
